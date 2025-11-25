541c38d0dadb809fba5a97bef0c293d7ab349512
illiayatsentyuk <illiayatsentyuk@gmail.com>
2025-11-25 18:56:34 +0200

    Update import for Analytics component in main.jsx to use the correct package from @vercel/analytics/react

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

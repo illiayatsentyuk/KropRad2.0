8229df74dfdeb92d042b8135ade34e262cb3c97f
illiayatsentyuk@gmail.com <illiayatsentyuk@gmail.com>
2025-11-26 20:51:24 +0200

    somtehiing

--- apps/frontend/src/pages/Article.jsx
+++ apps/frontend/src/pages/Article.jsx
@@ -1,16 +1,32 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 import { fetchArticleById } from "../api/article"
 import { BigArticle } from "../components/BigArticle"
 
 export const ArticlePage = () => {
     const { id } = useParams()
     const [article, setArticle] = useState(null)
+    
+    // Get articles from store
+    const articlesFromStore = useSelector((state) => state.articles.articles)
+    
     useEffect(() => {
-        fetchArticleById(id).then(data => {
-            setArticle(data)
-        })
-    }, [id])
+        const articleId = parseInt(id)
+        
+        // Check if article exists in store first
+        const cachedArticle = articlesFromStore.find(a => a.id === articleId)
+        
+        if (cachedArticle) {
+            console.log('📦 Using article from store')
+            setArticle(cachedArticle)
+        } else {
+            console.log('🌐 Fetching article from API')
+            fetchArticleById(articleId).then(data => {
+                setArticle(data)
+            })
+        }
+    }, [id, articlesFromStore])
 
     return (
         <div className="flex flex-col gap-4 w-full items-start justify-start py-8 px-4 sm:py-10 sm:px-6">
